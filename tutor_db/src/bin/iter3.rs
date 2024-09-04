faa2a847406afd7313355947ce7ef192176901f6
yifaaan <0219lyf@gmail.com>
2024-09-04 19:14:54 +0800

    add main for iter3

--- tutor_db/src/bin/iter3.rs
+++ tutor_db/src/bin/iter3.rs
@@ -1,3 +1,9 @@
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use routes::{course_routes, general_routes};
+use sqlx::PgPool;
+use state::AppState;
+use std::{env, io, sync::Mutex};
 #[path = "../iter3/db_access.rs"]
 mod db_access;
 #[path = "../iter3/handlers.rs"]
@@ -8,3 +14,25 @@ mod models;
 mod routes;
 #[path = "../iter3/state.rs"]
 mod state;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let db_poop = PgPool::connect(&database_url).await.unwrap();
+
+    let shared_data = web::Data::new(AppState {
+        health_check_response: "I'm good.".to_string(),
+        visit_count: Mutex::new(0),
+        db: db_poop,
+    });
+
+    let app = move || {
+        App::new()
+            .app_data(shared_data.clone())
+            .configure(general_routes)
+            .configure(course_routes)
+    };
+    HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
+}
