90d339ae7a86f7a7752be1efcc7c12b218d9d944
yifaaan <0219lyf@gmail.com>
2024-09-04 19:37:14 +0800

    fix get course db bug

--- tutor_db/src/iter3/db_access.rs
+++ tutor_db/src/iter3/db_access.rs
@@ -20,7 +20,8 @@ pub async fn get_courses_for_tutor_db(pool: &PgPool, tutor_id: i32) -> Vec<Cours
 
 pub async fn get_course_details_db(pool: &PgPool, tutor_id: i32, course_id: i32) -> Course {
     let course_row = sqlx::query!(
-        "SELECT tutor_id, course_id, course_name, posted_time FROM ezy_course_c4 where tutor_id = $1", tutor_id).fetch_one(pool).await.unwrap();
+        "SELECT tutor_id, course_id, course_name, posted_time FROM ezy_course_c4 where tutor_id = $1 and course_id = $2", tutor_id, course_id)
+        .fetch_one(pool).await.unwrap();
 
     Course {
         course_id: course_row.course_id,
