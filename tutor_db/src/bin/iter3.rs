90024d8723ab726858707c8d8e71b6edcafc376c
yifaaan <0219lyf@gmail.com>
2024-09-04 18:50:51 +0800

    add db_access fn

---
+++ tutor_db/src/bin/iter3.rs
@@ -0,0 +1,9 @@
+#[path = "../iter3/db_access.rs"]
+mod db_access;
+#[path = "../iter2/models.rs"]
+mod models;
+#[path = "../iter2/state.rs"]
+mod state;
+
+#[path = "../iter2/routes.rs"]
+mod routes;
