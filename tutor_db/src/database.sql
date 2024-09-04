b8c95111a276dba35c9f430c016ae091044e9881
yifaaan <0219lyf@gmail.com>
2024-09-04 15:41:35 +0800

    add database model

---
+++ tutor_db/src/database.sql
@@ -0,0 +1,17 @@
+drop table if exists ezy_course_c4;
+
+create table ezy_course_c4
+(
+    course_id serial primary key,
+    tutor_id INT not null,
+    course_name varchar(140) not null,
+    posted_time TIMESTAMP default now()
+);
+
+/* load seed data for testing */
+insert into ezy_course_c4
+    (course_id, tutor_id, course_name, posted_time)
+    values(1, 1, 'First course', '2024-9-4 15:40:00');
+insert into ezy_course_c4
+    (course_id, tutor_id, course_name, posted_time)
+    values(2, 1, 'Second course', '2024-9-4 15:41:00');
