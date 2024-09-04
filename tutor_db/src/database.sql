0505274e0abfb4952d4f3c7a04a356f38cec14ba
yifaaan <0219lyf@gmail.com>
2024-09-04 16:04:51 +0800

    update database.sql

--- tutor_db/src/database.sql
+++ tutor_db/src/database.sql
@@ -1,5 +1,6 @@
+/* Drop table if it already exists*/
 drop table if exists ezy_course_c4;
-
+/* Create a table. */
 create table ezy_course_c4
 (
     course_id serial primary key,
@@ -7,11 +8,11 @@ create table ezy_course_c4
     course_name varchar(140) not null,
     posted_time TIMESTAMP default now()
 );
+/* Load seed data for testing */
+insert into ezy_course_c4
+(course_id,tutor_id, course_name,posted_time)
+values(1, 1, 'First course', '2024-9-4 15:45:00');
 
-/* load seed data for testing */
 insert into ezy_course_c4
-    (course_id, tutor_id, course_name, posted_time)
-    values(1, 1, 'First course', '2024-9-4 15:40:00');
-insert into ezy_course_c4
-    (course_id, tutor_id, course_name, posted_time)
-    values(2, 1, 'Second course', '2024-9-4 15:41:00');
+(course_id, tutor_id, course_name,posted_time)
+values(2, 1, 'Second course', '2024-9-4 15:46:00');
