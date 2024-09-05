0f62a6ee54e13c8c91a6c707b3be0dc32b9a3bda
yifaaan <0219lyf@gmail.com>
2024-09-05 11:28:14 +0800

    update course struct

--- tutor_db/src/iter5/dbcripts/course.sql
+++ tutor_db/src/iter5/dbcripts/course.sql
@@ -40,3 +40,23 @@ values(1, 1, 'First course', '2024-9-5 05:40:00');
 insert into ezy_course_c5
     (course_id, tutor_id, course_name,posted_time)
 values(2, 1, 'Second course', '2021-9-5 05:45:00');
+
+
+
+
+drop table if exists ezy_course_c6;
+
+create table ezy_course_c6
+(
+    course_id serial primary key,
+    tutor_id INT not null,
+    course_name varchar(140) not null,
+    course_description varchar(2000),
+    course_format varchar(30),
+    course_structure varchar(200),
+    course_duration varchar(30),
+    course_price INT,
+    course_language varchar(30),
+    course_level varchar(30),
+    posted_time TIMESTAMP default now()
+);
--- tutor_db/src/iter5/models/course.rs
+++ tutor_db/src/iter5/models/course.rs
@@ -1,22 +1,97 @@
+use crate::errors::EzyTutorError;
 use actix_web::web;
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
+use std::convert::TryFrom;
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
+#[derive(Deserialize, Serialize, Debug, Clone, sqlx::FromRow)]
 pub struct Course {
+    // 数据库生成
     pub course_id: i32,
     pub tutor_id: i32,
     pub course_name: String,
+    pub course_description: Option<String>,
+    pub course_format: Option<String>,
+    pub course_structure: Option<String>,
+    pub course_duration: Option<String>,
+    pub course_price: Option<i32>,
+    pub course_language: Option<String>,
+    pub course_level: Option<String>,
+    // 数据库生成
     pub posted_time: Option<NaiveDateTime>,
 }
 
-impl From<web::Json<Course>> for Course {
-    fn from(value: web::Json<Course>) -> Self {
-        Self {
-            course_id: value.course_id,
+/// 客户端请求添加课程
+#[derive(Deserialize, Debug, Clone)]
+pub struct CreateCourse {
+    pub tutor_id: i32,
+    pub course_name: String,
+    pub course_description: Option<String>,
+    pub course_format: Option<String>,
+    pub course_structure: Option<String>,
+    pub course_duration: Option<String>,
+    pub course_price: Option<i32>,
+    pub course_language: Option<String>,
+    pub course_level: Option<String>,
+}
+
+impl From<web::Json<CreateCourse>> for CreateCourse {
+    fn from(value: web::Json<CreateCourse>) -> Self {
+        CreateCourse {
             tutor_id: value.tutor_id,
             course_name: value.course_name.clone(),
-            posted_time: value.posted_time,
+            course_description: value.course_description.clone(),
+            course_duration: value.course_duration.clone(),
+            course_format: value.course_format.clone(),
+            course_language: value.course_language.clone(),
+            course_level: value.course_level.clone(),
+            course_price: value.course_price,
+            course_structure: value.course_structure.clone(),
+        }
+    }
+}
+
+// impl TryFrom<web::Json<CreateCourse>> for CreateCourse {
+//     type Error = EzyTutorError;
+//     fn try_from(value: web::Json<CreateCourse>) -> Result<Self, Self::Error> {
+//         Ok(CreateCourse {
+//             tutor_id: value.tutor_id,
+//             course_name: value.course_name.clone(),
+//             course_description: value.course_description.clone(),
+//             course_duration: value.course_duration.clone(),
+//             course_format: value.course_format.clone(),
+//             course_language: value.course_language.clone(),
+//             course_level: value.course_level.clone(),
+//             course_price: value.course_price,
+//             course_structure: value.course_structure.clone(),
+//         })
+//     }
+// }
+
+/// 客户请求更新课程
+#[derive(Deserialize, Debug, Clone)]
+pub struct UpdateCourse {
+    pub course_name: String,
+    pub course_description: Option<String>,
+    pub course_format: Option<String>,
+    pub course_structure: Option<String>,
+    pub course_duration: Option<String>,
+    pub course_price: Option<i32>,
+    pub course_language: Option<String>,
+    pub course_level: Option<String>,
+}
+
+impl From<web::Json<UpdateCourse>> for UpdateCourse {
+    fn from(value: web::Json<UpdateCourse>) -> Self {
+        Self {
+            course_name: value.course_name.clone(),
+            course_description: value.course_description.clone(),
+            course_duration: value.course_duration.clone(),
+            course_format: value.course_format.clone(),
+            course_language: value.course_language.clone(),
+            course_level: value.course_level.clone(),
+            course_price: value.course_price,
+            course_structure: value.course_structure.clone(),
         }
     }
 }
