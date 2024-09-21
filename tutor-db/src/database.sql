4340c2e403644ca9a9b953d353d0848e086ad38f
yifaaan <0219lyf@gmail.com>
2024-09-21 16:04:20 +0800

    add error handler

--- tutor-db/src/database.sql
+++ tutor-db/src/database.sql
@@ -1,4 +1,4 @@
-
+DROP TABLE IF EXISTS ezy_course_c4;
 CREATE TABLE ezy_course_c4
 (
     course_id SERIAL PRIMARY KEY,
@@ -12,4 +12,24 @@ INSERT INTO ezy_course_c4 (course_id, tutor_id, course_name, posted_time)
 VALUES(1, 1, 'Frist course', '2024-9-21 05:40:00');
 
 INSERT INTO ezy_course_c4 (course_id, tutor_id, course_name, posted_time)
+VALUES(2, 1, 'Second course', '2024-9-21 05:45:00');
+
+
+
+
+/* add errors */
+DROP TABLE IF EXISTS ezy_course_c5;
+CREATE TABLE ezy_course_c5
+(
+    course_id SERIAL PRIMARY KEY,
+    tutor_id INT NOT NULL,
+    course_name VARCHAR(140) NOT NULL,
+    posted_time TIMESTAMP DEFAULT now()
+);
+
+
+INSERT INTO ezy_course_c5 (course_id, tutor_id, course_name, posted_time)
+VALUES(1, 1, 'Frist course', '2024-9-21 05:40:00');
+
+INSERT INTO ezy_course_c5 (course_id, tutor_id, course_name, posted_time)
 VALUES(2, 1, 'Second course', '2024-9-21 05:45:00');
---
+++ tutor-db/src/iter4/errors.rs
@@ -0,0 +1,68 @@
+use std::fmt;
+
+use actix_web::{http::StatusCode, HttpResponse, ResponseError};
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub enum EzyTutorError {
+    DBError(String),
+    ActixError(String),
+    NotFound(String),
+}
+
+#[derive(Debug, Serialize)]
+pub struct MyErrorResponse {
+    error_message: String,
+}
+
+impl EzyTutorError {
+    /// convert a custom error struct to a user-friendly text message
+    fn error_response(&self) -> String {
+        match self {
+            Self::DBError(msg) => {
+                println!("Database error occurred: {:?}", msg);
+                "Database error".into()
+            }
+            Self::ActixError(msg) => {
+                println!("Server error occurred: {:?}", msg);
+                "Internal server error".into()
+            }
+            Self::NotFound(msg) => {
+                println!("Not found error occurred: {:?}", msg);
+                msg.into()
+            }
+        }
+    }
+}
+
+impl ResponseError for EzyTutorError {
+    fn status_code(&self) -> actix_web::http::StatusCode {
+        match self {
+            Self::DBError(msg) | Self::ActixError(msg) => StatusCode::INTERNAL_SERVER_ERROR,
+            Self::NotFound(msg) => StatusCode::NOT_FOUND,
+        }
+    }
+    fn error_response(&self) -> actix_web::HttpResponse<actix_web::body::BoxBody> {
+        HttpResponse::build(self.status_code()).json(MyErrorResponse {
+            error_message: self.error_response(),
+        })
+    }
+}
+
+impl fmt::Display for EzyTutorError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+impl From<actix_web::error::Error> for EzyTutorError {
+    fn from(value: actix_web::error::Error) -> Self {
+        EzyTutorError::ActixError(value.to_string())
+    }
+}
+
+impl From<sqlx::error::Error> for EzyTutorError {
+    fn from(value: sqlx::error::Error) -> Self {
+        EzyTutorError::DBError(value.to_string())
+    }
+}
