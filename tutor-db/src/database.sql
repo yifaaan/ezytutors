be35fc3188632f27715539c1129888decb456b92
yifaaan <0219lyf@gmail.com>
2024-09-21 11:04:28 +0800

    database set up

---
+++ tutor-db/src/database.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE ezy_course_c4
+(
+    course_id SERIAL PRIMARY KEY,
+    tutor_id INT NOT NULL,
+    course_name VARCHAR(140) NOT NULL,
+    posted_time TIMESTAMP DEFAULT now()
+);
+
+/* for test */
+INSERT INTO ezy_course_c4 (course_id, tutor_id, course_name, posted_time)
+VALUES(1, 1, 'Frist course', '2024-9-21 05:40:00');
+
+INSERT INTO ezy_course_c4 (course_id, tutor_id, course_name, posted_time)
+VALUES(2, 1, 'Second course', '2024-9-21 05:45:00');
