84a7ff516f96a686aa40e2e32dc5811c8ac05b4c
yifaaan <0219lyf@gmail.com>
2024-09-22 10:29:16 +0800

    add user input with form

---
+++ tutor-web-app-ssr/src/bin/iter2.rs
@@ -0,0 +1,58 @@
+use std::env;
+
+use actix_web::web::Data;
+use actix_web::{error, web, App, Error, HttpResponse, HttpServer, Result};
+use serde::{Deserialize, Serialize};
+use tera::Tera;
+
+// store tera template in application state
+async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+    let s = tmpl
+        .render("form.html", &tera::Context::new())
+        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
+
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Tutor {
+    name: String,
+}
+// curl -X POST localhost:8080/tutors -d "name=Terry"
+async fn handle_post_tutor(
+    tmpl: web::Data<tera::Tera>,
+    params: web::Form<Tutor>,
+) -> Result<HttpResponse, Error> {
+    let mut ctx = tera::Context::new();
+    ctx.insert("name", &params.name);
+    ctx.insert("text", "Welcome!");
+    let s = tmpl
+        .render("user.html", &ctx)
+        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
+
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let addr = env::var("SERVER_ADDR").unwrap_or_else(|_| "127.0.0.1:8080".to_string());
+    // println!("Listening on: 127.0.0.1:8080");
+    println!("Listening on: {}", addr);
+    HttpServer::new(|| {
+        let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/static/iter2/**/*")).unwrap();
+
+        App::new().app_data(Data::new(tera)).configure(app_config)
+    })
+    // .bind("127.0.0.1:8080")?
+    .bind(addr)?
+    .run()
+    .await
+}
+
+fn app_config(config: &mut web::ServiceConfig) {
+    config.service(
+        web::scope("")
+            .service(web::resource("/").route(web::get().to(index)))
+            .service(web::resource("/tutors").route(web::post().to(handle_post_tutor))),
+    );
+}
