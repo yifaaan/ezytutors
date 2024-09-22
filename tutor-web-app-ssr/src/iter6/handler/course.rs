c3343e46dcc7e91419c118d7eda759e116179f1e
yifaaan <0219lyf@gmail.com>
2024-09-22 15:51:26 +0800

    add post new course handler

--- tutor-web-app-ssr/src/iter6/handler/course.rs
+++ tutor-web-app-ssr/src/iter6/handler/course.rs
@@ -1,13 +1,42 @@
 use actix_web::{web, Error, HttpResponse};
+use serde_json::json;
 
-use crate::iter6::state::AppState;
+use crate::iter6::{
+    model::{NewCourse, NewCourseResponse},
+    state::AppState,
+};
 
 pub async fn handle_insert_course(
     _tmpl: web::Data<tera::Tera>,
     _app_state: web::Data<AppState>,
+    path: web::Path<i32>,
+    params: web::Json<NewCourse>,
 ) -> Result<HttpResponse, Error> {
-    println!("Got insert request");
-    Ok(HttpResponse::Ok().body("Got insert request"))
+    let tutor_id = path.into_inner();
+    let new_course = json!({
+        "tutor_id": tutor_id,
+        "course_name": &params.course_name,
+        "course_description": &params.course_description,
+        "course_format": &params.course_format,
+        "course_structure": &params.course_structure,
+        "course_duration": &params.course_duration,
+        "course_price": &params.course_price,
+        "course_language": &params.course_language,
+        "course_level": &params.course_level
+    });
+
+    // 向tutor web server发送注册数据
+    let awc_client = awc::Client::default();
+    let res = awc_client
+        .post("http://localhost:3000/courses/")
+        .send_json(&new_course)
+        .await
+        .unwrap()
+        .body()
+        .await?;
+    println!("Finished call: {:?}", res);
+    let course_response: NewCourseResponse = serde_json::from_str(&std::str::from_utf8(&res)?)?;
+    Ok(HttpResponse::Ok().json(course_response))
 }
 
 pub async fn handle_update_course(
--- tutor-web-app-ssr/src/iter6/model.rs
+++ tutor-web-app-ssr/src/iter6/model.rs
@@ -1,3 +1,4 @@
+use actix_web::web;
 use serde::{Deserialize, Serialize};
 
 ///  data captured in the tutor registration form
@@ -31,4 +32,50 @@ pub struct User {
 pub struct TutorSigninForm {
     pub username: String,
     pub password: String,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct NewCourse {
+    pub course_name: String,
+    pub course_description: String,
+    pub course_format: String,
+    pub course_duration: String,
+    pub course_structure: Option<String>,
+    pub course_price: Option<i32>,
+    pub course_language: Option<String>,
+    pub course_level: Option<String>,
+}
+
+/// 从tutor web server数据库服务中发来的数据
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct NewCourseResponse {
+    pub course_id: i32,
+    pub tutor_id: i32,
+    pub course_name: String,
+    pub course_description: String,
+    pub course_format: String,
+    pub course_structure: Option<String>,
+    pub course_duration: String,
+    pub course_price: Option<i32>,
+    pub course_language: Option<String>,
+    pub course_level: Option<String>,
+    pub posted_time: String,
+}
+
+impl From<web::Json<NewCourseResponse>> for NewCourseResponse {
+    fn from(new_course: web::Json<NewCourseResponse>) -> Self {
+        NewCourseResponse {
+            tutor_id: new_course.tutor_id,
+            course_id: new_course.course_id,
+            course_name: new_course.course_name.clone(),
+            course_description: new_course.course_description.clone(),
+            course_format: new_course.course_format.clone(),
+            course_structure: new_course.course_structure.clone(),
+            course_duration: new_course.course_duration.clone(),
+            course_price: new_course.course_price,
+            course_language: new_course.course_language.clone(),
+            course_level: new_course.course_level.clone(),
+            posted_time: new_course.posted_time.clone(),
+        }
+    }
 }
